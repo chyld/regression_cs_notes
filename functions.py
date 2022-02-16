96201cc1761e40de7537ea8bd7b9d3af559e906d
Chyld Medford <chyld.medford@gmail.com>
2022-02-15 23:31:09 -0800

    update

--- functions.py
+++ functions.py
@@ -1,6 +1,14 @@
-def clean(df):
-    df = df.copy()
-    df = df.loc[:, ['YearMade', 'MachineHoursCurrentMeter']]
-    mhcm_mean = df.MachineHoursCurrentMeter.mean()
-    df.MachineHoursCurrentMeter = df.MachineHoursCurrentMeter.fillna(mhcm_mean)
-    return df
+# Used to prepare dataframes for analysis
+
+class Cleaner:
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, train):
+        self.mhcm_mean = train.MachineHoursCurrentMeter.mean()
+
+    def transform(self, df):
+        df = df.copy()
+        df = df.loc[:, self.columns]
+        df.MachineHoursCurrentMeter = df.MachineHoursCurrentMeter.fillna(self.mhcm_mean)
+        return df
