1d9f32eae61b9a367b707b91e1ccec59b3dc6b3b
Chyld Medford <chyld.medford@gmail.com>
2022-02-16 07:16:49 -0800

    update

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 # Used to prepare dataframes for analysis
 
+import numpy as np
+from sklearn.metrics import make_scorer
+
 class Cleaner:
     def __init__(self, columns):
         self.columns = columns
@@ -12,3 +15,9 @@ class Cleaner:
         df = df.loc[:, self.columns]
         df.MachineHoursCurrentMeter = df.MachineHoursCurrentMeter.fillna(self.mhcm_mean)
         return df
+
+def rmsle_temp(actual, predictions):
+    log_diff = np.log(predictions+1) - np.log(actual+1)
+    return np.sqrt(np.mean(log_diff**2))
+
+rmsle = make_scorer(rmsle_temp, greater_is_better=False)
